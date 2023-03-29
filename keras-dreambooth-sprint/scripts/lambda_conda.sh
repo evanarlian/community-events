e99efef3b612b48fd9e775c19b7fe31b0ad67b42
Evan Arlian <evanarlian2000@gmail.com>
2023-03-29 14:34:24 +0700

    install kerascv

--- keras-dreambooth-sprint/scripts/lambda_conda.sh
+++ keras-dreambooth-sprint/scripts/lambda_conda.sh
@@ -9,3 +9,23 @@ eval "$(/home/ubuntu/miniconda3/bin/conda shell.bash hook)"
 conda init
 conda config --set auto_activate_base false
 source ~/.bashrc
+
+# setup cuda stuff and install keras_cv
+# https://github.com/huggingface/community-events/pull/132
+conda create -n kerascv python=3.10 -y
+conda activate kerascv
+conda install nb_conda_kernels -y
+ipython kernel install --user --name=kerascv
+conda install -c conda-forge cudatoolkit=11.8.0 -y
+python3 -m pip install nvidia-cudnn-cu11==8.6.0.163
+
+echo "export XLA_FLAGS=--xla_gpu_cuda_data_dir=/usr/lib/cuda" >> .bashrc
+CUDNN_PATH=$(dirname $(python -c "import nvidia.cudnn;print(nvidia.cudnn.__file__)"))
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/:$CUDNN_PATH/lib" >> .bashrc
+source ~/.bashrc
+
+python -m pip install tensorflow==2.12.*
+pip install keras_cv===0.4.2 tensorflow_datasets===4.8.1 pillow===9.4.0 imutils opencv-python matplotlib huggingface-hub pycocotools
+
+# check, should be printing at least one gpu device
+python -c "import tensorflow as tf; print(tf.config.list_physical_devices('GPU')); print(tf.__version__)"
